45200660894477abe7c2b93b92b2b7babc3893eb
Sasha Moleva <aleksmoleva@yandex.ru>
2020-08-27 22:03:03 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let someStr = 'Изменил, сохранил, собрал';
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+console.log(doubledNumbers);
+import "./style.css";
